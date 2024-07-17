e77e0745591ffc2fe0cebedc66f63dbfd97cc68c
Sudaryatno <sudaryatno@eudeka.id>
2024-07-17 11:10:28 +0700

    add: documentation

--- js/balancedBracket.js
+++ js/balancedBracket.js
@@ -1,3 +1,14 @@
+/** 
+ * Aturan:
+ * 1. Tanda braket / karakter yang diperbolehkan sebagai berikut: ( , ) , { , } , atau [ , ]. 
+ * 2. Bracket bisa dipisahkan dengan atau tanpa whitespace.
+ * 3. Periksa tanda kurung yang memiliki kecocokan antara braket buka dan braket tutup dengan mengembalikan nilai string YES atau NO.
+ * 
+ * Soal:
+ * 1. Buat fungsi untuk menemukan Balanced Bracket dengan kompleksitas paling rendah!
+ * 2. Jelaskan kompleksitas dari penyelesaianmu untuk No.3 dan cantumkan di README Repo! 
+*/
+
 function balanceBracket(a) {
     // code disini
     return true;
