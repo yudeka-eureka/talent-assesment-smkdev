6536c3556f26a0fa15dcb21076acfd8c0183afc2
lost silvian <adihidayatullah96.ah@gmail.com>
2024-08-04 23:12:12 +0700

    feat: add weightedString function and fix unit tests

--- js/weightedStrings.js
+++ js/weightedStrings.js
@@ -18,7 +18,28 @@
 
 function weightedString(a = 'abbcccd', b = [1, 3, 9, 8]) {
     // code disini
-    return ["YES", "YES", "YES", "NO"];
+    const alphabetWeight = (char) => char.charCodeAt(0) - 96;
+
+    let weights = new Set();
+
+    let currentChar = a[0];
+    let currentCount = 1;
+    weights.add(alphabetWeight(currentChar));
+
+    for (let i = 1; i < a.length; i++) {
+        if (a[i] === currentChar) {
+            currentCount++;
+        } else {
+            currentChar = a[i];
+            currentCount = 1;
+        }
+        for (let j = 1; j <= currentCount; j++) {
+            weights.add(alphabetWeight(currentChar) * j);
+        }
+    }
+
+    // return ["YES", "YES", "YES", "NO"];
+    return b.map(value => weights.has(value) ? "YES" : "NO");
 }
 
 module.exports = weightedString;
