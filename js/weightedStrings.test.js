1b996068a6ca96ebb5c925d029f99113b63664a1
Bagas Arisandi Hidayat <bagassandi13@gmail.com>
2024-08-09 09:20:13 +0530

    fix: fix expected bug in weightedStrings.test

--- js/weightedStrings.test.js
+++ js/weightedStrings.test.js
@@ -9,5 +9,5 @@ test('weightedString with input "aaabbbcccddd" and [5, 9, 7, 8, 12, 5] to equal 
 });
 
 test('weightedString with input "getitssimple" and [1, 2, 8, 7, 12, 38, 10] to equal [NO, NO, NO, YES, YES, YES, NO]', () => {
-    expect(weightedString("getitssimple", [1, 2, 8, 7, 12, 38, 10])).toEqual([NO, NO, NO, YES, YES, YES, NO]);
+    expect(weightedString("getitssimple", [1, 2, 8, 7, 12, 38, 10])).toEqual(["NO", "NO", "NO", "YES", "YES", "YES", "NO"]);
 });
