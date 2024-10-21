feb616c256253baddc931902d9d4d789d19041fc
Sudaryatno <sudaryatno@eudeka.id>
2024-10-21 08:20:03 +0700

    feat:add java test

---
+++ java-test/app/src/main/java/dev/smk/java/app/BalancedBracket.java
@@ -0,0 +1,9 @@
+
+package dev.smk.java.app;
+import java.util.*;
+public class BalancedBracket {
+
+    public static String isBalanced(String s) {
+        return "YES";
+    }
+}
---
+++ java-test/app/src/main/java/dev/smk/java/app/HighestPalindrome.java
@@ -0,0 +1,9 @@
+
+package dev.smk.java.app;
+import java.util.*;
+public class HighestPalindrome {
+
+    public static String highestPalindrome(String s, int k) {
+        return "1344";
+    }
+}
